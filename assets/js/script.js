e8cc4ee7d0501c5bc84f100e0e3b19eb9068707b
CeciliaSwe <cedergren.cecilia@gmail.com>
2021-06-13 12:26:09 +0000

    First function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//Wait for DOM to load, then run game
+//Get button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button"); 
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked the ${gameType}!`);
+            }
+        });
+    }
+});
+
 function runGame(){
 
 }
